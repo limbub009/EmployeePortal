1886b7b1d4c36c4c0ca0b3d7f15b0c340a482110
rez789 <60137727+rez789@users.noreply.github.com>
2021-04-04 17:34:56 +0100

    Update view.php

--- view.php
+++ view.php
@@ -2,6 +2,26 @@
 
 require ("dbc.php");
 
+function check_login($con){
+    #check if session value is set
+    if(isset($_SESSION['id']))
+    {
+        $id = $_SESSION['id'];
+        $query = "select * from login where user_id = '$id' limit 1";
+
+        $result = mysqli_query($con, $query);
+        if($result && mysqli_num_rows($result) > 0)
+        {
+            $user_data = mysqli_fetch_assoc($result);
+            return $user_data;
+        }
+    }
+
+    #redirect to login
+    header("location: login.php");
+    die; #stop the code here
+}
+
 function getUsersData($id){
     $array = array();
     $q = mysqli_query("SELECT * FROM 'users' WHERE 'ID' =".$id);
