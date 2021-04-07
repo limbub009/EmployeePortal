e27877850b5ce1267aeaa65e4a27905c1a5801f4
limbub009 <60136808+limbub009@users.noreply.github.com>
2021-04-07 18:22:42 +0100

    EDIT PROFILE
    functionality complete, just need to implement styling

---
+++ editprofile.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Edit Profile </title>
+  <h1> Edit Profile </h1>
+</head>
+<body>
+  <form action="updateprofile.php" method="post">
+     name: <input type="text" name="name"><br>
+     email: <input type="email" name="email"><br>
+     phone: <input type="int" name="phone"><br>
+     departmentid: <input type="int" name="deptid"><br>
+     <input type="submit" name="edit">
+</body>
+</html>
---
+++ updateprofile.php
@@ -0,0 +1,50 @@
+<?php
+
+ session_start();
+ include("connections.php");
+ include("functions.php");
+
+ echo $_SESSION['id'];
+
+ $username = $_POST['name'];
+ if(isset($_POST['edit']))
+ {
+    $id=$_SESSION['id'];
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $phone=$_POST['phone'];
+    $deptid=$_POST['deptid'];
+
+
+
+    $select= "select * from employee where user_id='$id'";
+    $sql = mysqli_query($con,$select);
+    $row = mysqli_fetch_assoc($sql);
+    $res= $row['user_id'];
+    echo $row['user_id'];
+    echo $res;
+    if($res === $id)
+    {
+
+       $update = "update employee set name='$name',email='$email',phone='$phone', departmentid = '$deptid' where user_id='$id'";
+       $sql2 = mysqli_query($con,$update);
+
+
+if($sql2)
+       {
+           /*Successful*/
+           /*header('location:dashboard.php');
+       }
+       else
+       {
+           /*sorry your profile is not update*/
+           /*header('location:editprofile.php');*/
+       }
+    }
+    else
+    {
+        /*sorry your id is not match*/
+        /*header('location:editprofile.php');*/
+    }
+ }
+?>
