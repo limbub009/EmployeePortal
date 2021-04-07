2b4736cbe20cd34a71e62f208dc6d426ddcd0a18
saakshisaxena <saakshi.saxena2000@gmail.com>
2021-04-07 19:32:54 +0100

    View data bug free
    Can view all names from database. DB object reacted.

---
+++ searchIndex.php
@@ -0,0 +1,34 @@
+<?php
+  require_once('DB.php');
+
+  $db = new DB();
+  $data = $db->viewData();
+
+  // var_dump($data);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Live Search</title>
+</head>
+
+<body>
+
+  <h1>Live Search</h1>
+
+  <form action="search.php" method="POST">
+    <input type="text" name="name" placeholder="Search Here..." id="searchBox">
+  </form>
+
+  <ul id="dataViewer">
+  <?php foreach ($data as $i) { ?>
+    <li><?php echo $i["name"]; ?></li>
+  <?php } ?>
+  </ul>
+
+</body>
+</html>
