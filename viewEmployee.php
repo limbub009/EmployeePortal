3f8d92eef120a93d6a09feeda199ba97f3f7b562
rez789 <60137727+rez789@users.noreply.github.com>
2021-04-07 17:41:20 +0100

    Update viewEmployee.php

--- viewEmployee.php
+++ viewEmployee.php
@@ -16,10 +16,12 @@
     <link href="viewEmployee.css" rel="stylesheet">
   </head>
   <body>
+  <?php
+  if(!isset($_SESSION['searchName']))
+  {
+  ?>
     <div class="page-content page-container" id="page-content">
-        <?php
-        if(!isset($_SESSION['searchName'])){
-        ?>
+
         <div class="padding">
             <div class="row container d-flex justify-content-center">
                 <div class="col-xl-6 col-md-12">
@@ -28,53 +30,61 @@
                             <div class="col-sm-4 bg-c-lite-green user-profile">
                                 <div class="card-block text-center text-white">
                                     <div class="m-b-25"> <img src="https://img.icons8.com/bubbles/100/000000/user.png" class="img-radius" alt="User-Profile-Image"> </div>
-                                <?php if(isset($_SESSION['searchName'])){
-                                    $userData = getUserData(getID($_SESSION['searchName']))
-                                    ?>
-                                    <h6 class="f-w-600"><?php echo $userData['name']; ?></h6>
+                                <?php if(isset($_SESSION['searchName'])) {
+      $userData = getUserData(getID($_SESSION['searchName']))
+      ?>
+      <h6 class="f-w-600"><?php echo $userData['name']; ?></h6>
 
-                                </div>
-                            </div>
-                            <div class="col-sm-8">
-                                <div class="card-block">
-                                    <div class="row topCard">
-                                        <div class="col-sm-6">
-                                            <p class="m-b-10 f-w-600">Email</p>
-                                            <h6 class="text-muted f-w-400"><?php echo $userData['email']; ?></h6>
+      </div>
+      </div>
+      <div class="col-sm-8">
+          <div class="card-block">
+              <div class="row topCard">
+                  <div class="col-sm-6">
+                      <p class="m-b-10 f-w-600">Email</p>
+                      <h6 class="text-muted f-w-400"><?php echo $userData['email']; ?></h6>
 
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <p class="m-b-10 f-w-600">Phone</p>
-                                            <h6 class="text-muted f-w-400"><?php echo $userData['number'];?></h6>
+                  </div>
+                  <div class="col-sm-6">
+                      <p class="m-b-10 f-w-600">Phone</p>
+                      <h6 class="text-muted f-w-400"><?php echo $userData['number']; ?></h6>
 
-                                        </div>
-                                    </div>
-                                    <div class="row bottomCard">
-                                        <div class="col-sm-6">
-                                            <p class="m-b-10 f-w-600">Department</p>
-                                            <h6 class="text-muted f-w-400"><?php echo $userData['department'];?></h6>
+                  </div>
+              </div>
+              <div class="row bottomCard">
+                  <div class="col-sm-6">
+                      <p class="m-b-10 f-w-600">Department</p>
+                      <h6 class="text-muted f-w-400"><?php echo $userData['department']; ?></h6>
 
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <p class="m-b-10 f-w-600">Job Role</p>
-                                            <h6 class="text-muted f-w-400">Dinoter husainm</h6>
-                                        </div>
-                                    </div>
-                                    <ul class="social-link list-unstyled m-t-40 m-b-10">
-                                        <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
-                                        <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
-                                        <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php
-          }
-        ?>
-    </div>
+                  </div>
+                  <div class="col-sm-6">
+                      <p class="m-b-10 f-w-600">Job Role</p>
+                      <h6 class="text-muted f-w-400">Dinoter husainm</h6>
+                  </div>
+              </div>
+              <ul class="social-link list-unstyled m-t-40 m-b-10">
+                  <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook"
+                         data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook"
+                                            aria-hidden="true"></i></a></li>
+                  <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter"
+                         data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a>
+                  </li>
+                  <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
+                         data-original-title="instagram" data-abc="true"><i
+                              class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+    <?php
+        }
+        }
+    ?>
   </body>
 </html>
