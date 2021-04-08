9f37670a569b5151ee0c18e04bee205d16cdc916
saakshisaxena <saakshi.saxena2000@gmail.com>
2021-04-08 11:24:36 +0100

    bug fix

--- main.js
+++ main.js
@@ -20,8 +20,32 @@ function viewSearchResult(data) {
   dataViewer.innerHTML = "";
 
   for(let i=0; i<data.length; i++) {
+    console.log("Hi");
     const li = document.createElement("li");
     li.innerHTML = data[i]["name"];
+    li.onclick = function() {postSearchName()};
     dataViewer.appendChild(li);
   }
 }
+
+function postSearchName() {
+  alert("bye");
+  fetch('viewEmployee.php', {
+    method: 'POST',
+    body: new URLSearchParams('searchname='+name)
+  })
+}
+
+// $('li').on('click', function() {
+//     console.log("Bye");
+//      alert(document.getElementsByTagName('li')[0].innerHTML);
+//      alert("bye");
+// });
+
+// $(document).ready(function()
+//  {
+//   $("ul").on("click","li", function(){
+//     alert("Hello");
+//      // alert($(this).find("span.t").text());
+//   });
+// });
