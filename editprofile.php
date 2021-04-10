b0fbc4c5f33dbc0b3a9ef0b59042f1f262deb172
limbub009 <60136808+limbub009@users.noreply.github.com>
2021-04-10 18:27:56 +0100

    EDIT PROFILE || fully complete || CSS DONE
    css and functionality done

--- editprofile.php
+++ editprofile.php
@@ -4,13 +4,16 @@
 <head>
   <title>Edit Profile </title>
   <h1> Edit Profile </h1>
+  <link rel="stylesheet" href="editprofile.css">
 </head>
 <body>
+  <div>
   <form action="updateprofile.php" method="post">
-     name: <input type="text" name="name"><br>
-     email: <input type="email" name="email"><br>
-     phone: <input type="int" name="phone"><br>
-     departmentid: <input type="int" name="deptid"><br>
+     Name: <input type="text" name="name"><br>
+     E-mail: <input type="email" name="email"><br>
+     Phone: <input type="int" name="phone"><br>
+     Dept Id: <input type="int" name="deptid"><br>
      <input type="submit" name="edit">
+   </div>
 </body>
 </html>
--- updateprofile.php
+++ updateprofile.php
@@ -4,7 +4,7 @@
  include("connections.php");
  include("functions.php");
 
- echo $_SESSION['id'];
+ echo $_SESSION['id']; //get current user id
 
  $username = $_POST['name'];
  if(isset($_POST['edit']))
@@ -15,33 +15,30 @@
     $phone=$_POST['phone'];
     $deptid=$_POST['deptid'];
 
-
-
     $select= "select * from employee where user_id='$id'";
-    $sql = mysqli_query($con,$select);
-    $row = mysqli_fetch_assoc($sql);
-    $res= $row['user_id'];
-    echo $row['user_id'];
-    echo $res;
-    if($res === $id)
+    $sqlquery = mysqli_query($con,$select);
+
+    $rowselect = mysqli_fetch_assoc($sqlquery);
+    $result= $rowselect['user_id'];
+
+    echo $rowselect['user_id'];
+    echo $result;
+
+    if($result === $id)
     {
 
        $update = "update employee set name='$name',email='$email',phone='$phone', departmentid = '$deptid' where user_id='$id'";
-       $sql2 = mysqli_query($con,$update);
+       $sqlquery2 = mysqli_query($con,$update);
 
 
-if($sql2)
+       if($sqlquery2)
        {
               header('location:dashboard.php');
        }
-       else
-       {
-              /*failed*/
-       }
-    }
+     }
     else
     {
-        /*failed*/
+        echo "failed";
     }
  }
 ?>
