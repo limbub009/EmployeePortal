1ab3b67928ed874773119318fbb7e627ece1f5af
rez789 <60137727+rez789@users.noreply.github.com>
2021-04-12 20:55:44 +0100

    added connection tests

--- viewEmp.php
+++ viewEmp.php
@@ -3,6 +3,7 @@
 session_start();
 include("connections.php");
 include("functions.php");
+$user_data = check_login($con);
 ?>
 <html>
 <head>
@@ -11,36 +12,49 @@ include("functions.php");
     <link rel="stylesheet" type="text/css" href="viewEmp.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Employee Portal</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div id="linksnav" class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Your Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">View Schedule</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Search Employee</a>
-                    </li>
-                </ul>
-                <form class="d-flex">
-                    <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
-                    <button class="btn btn-outline-success" type="submit" style="margin-right: 10%;">LogIn</button>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Employee Portal</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <!-- ADD PHP TO MAKE IT VISIBLE ONLY WHEN USER IS LOGGED IN-->
+                <li class="nav-item">
+                    <a class="nav-link" href="dashboard.php">Your Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">View Schedule</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Search Employee</a>
+                </li>
+
+            </ul>
+            <form class="d-flex">
+                <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
+
+
+                <?php if(!isset($_SESSION['id']) || empty($_SESSION['id'])){ ?>
+                    <button id="logoutbutton" class="btn btn-outline-success" type="submit" style="margin-right: 7%;"><a href='login.php' style="text-decoration: none; color: white;">LogIn</a></button>
+
+                <?php } else { ?>
+
+                    <button id="loginbutton" class="btn btn-outline-success" type="submit" style="margin-right: 7%;"><a href='logout.php' style="text-decoration: none; color: white;">LogOut</a></button>
                     <!-- we can do a signIn forum fro admin's eyes only !!! ADD PHP SCRIPT HERE FOR SIGNIN BUTTON!!!-->
-                    <button class="btn btn-outline-success" type="submit">SignIn</button>
-                </form>
-            </div>
+                <?php } ?>
+
+                <button class="btn btn-outline-success" type="submit"><a style="text-decoration: none; color: white;" href='createAccount.php'>Create Account</a></button>
+
+
+            </form>
         </div>
-    </nav>
+    </div>
+</nav>
     <div class="main">
         <div class="image">
             <?php if(isset($_GET['searchname'])) {
