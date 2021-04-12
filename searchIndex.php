dc532b69e3e39e80e78c4e0277d24b4199ed4fdf
limbub009 <60136808+limbub009@users.noreply.github.com>
2021-04-12 21:28:10 +0100

    fixed logout in navbar searchindex

--- searchIndex.php
+++ searchIndex.php
@@ -28,43 +28,8 @@ session_start();
 
 <body>
   <header>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Employee Portal</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <!-- ADD PHP TO MAKE IT VISIBLE ONLY WHEN USER IS LOGGED IN-->
-            <li class="nav-item">
-              <a class="nav-link" href="dashboard.php">Your Profile</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">View Schedule</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Search Employee</a>
-            </li>
-            <!-- <li class="nav-item">
-              <a class="nav-link" href="#">About Us</a>
-            </li> -->
-            <!-- <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li> -->
-          </ul>
-          <form class="d-flex">
-            <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
-            <button class="btn btn-outline-success" type="submit" style="margin-right: 7%;"><a href='login.php' style="text-decoration: none; color: white;">LogIn</a></button>
-            <!-- we can do a signIn forum fro admin's eyes only !!! ADD PHP SCRIPT HERE FOR SIGNIN BUTTON!!!-->
-            <button class="btn btn-outline-success" type="submit"><a style="text-decoration: none; color: white;" href='createAccount.php'>Create Account</a></button>
-          </form>
-        </div>
-      </div>
-    </nav>
+
+    <?php include('navbar.php'); ?>
   </header>
 
   <h1>Live Search</h1>
